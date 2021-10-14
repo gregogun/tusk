7158f484c63239a226828057a2c77eab64200852
Greg Ogun <gregogun97@gmail.com>
2021-10-15 00:02:43 +0100

    chore: add types

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,6 @@
-import React from 'react';
 import { CSS } from 'stitches.config';
 
-export interface IconChild extends React.SVGAttributes<SVGElement> {
+export interface IconProps extends React.SVGAttributes<SVGElement> {
   children?: never;
   css?: CSS;
-}
-
-export interface IconProps {
-  children: IconChild;
-  as: React.FC;
 }
