dc296173bfc0a7e5aa1a5a14ed92e3db9af4bc24
Greg Ogun <gregogun97@gmail.com>
2021-10-15 00:01:51 +0100

    feat: add next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,10 @@
-import NextAuth from 'next-auth';
+/* eslint-disable no-param-reassign */
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
 import { NextApiHandler } from 'next';
+import { getSession } from 'next-auth/react';
 
 const options = {
   adapter: PrismaAdapter(prisma),
@@ -12,13 +14,28 @@ const options = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  cookie: {
-    secure: process.env.NODE_ENV && process.env.NODE_ENV === 'production',
-  },
+  // cookies: {
+  //   secure: process.env.NODE_ENV && process.env.NODE_ENV === 'production',
+  // },
   session: {
     jwt: true,
     maxAge: 30 * 24 * 60 * 60,
   },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.userId = token.uid;
+      //console.log(session);
+      return Promise.resolve(session);
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.uid = user.id;
+      }
+      //console.log(token);
+      return Promise.resolve(token);
+    },
+  },
+  // database: process.env.DATABASE_URL,
 };
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
