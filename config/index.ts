97df56a21f4af988a423de4b8ce9f27ed4a39606
Greg Ogun <gregogun97@gmail.com>
2021-10-21 13:05:09 +0100

    refactor: update url

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://tusk.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://tuskk.vercel.app';
